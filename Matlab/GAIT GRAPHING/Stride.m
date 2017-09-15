3f6184770c3a6d47088360eac3ba7faeec9a05bb
maxnussbaum <maxnussbaum@live.com>
2017-09-15 13:56:41 -0400

    Remove Duplicates
    Remove Duplicates

--- Matlab/GAIT GRAPHING/Stride.m
+++
@@ -1,7 +0,0 @@
-function[Subject] = Stride(Subject,Name)
-Subject.Stridecm = 100*(Subject.KneeRightZ - Subject.KneeLeftZ);
-Subject.ApproxStride = abs(Subject.KneeRightZ - Subject.KneeLeftZ);
-Subject.MaxApproxStride = max(Subject.ApproxStride);
-Subject.MaxApproxStridecm = 100*Subject.MaxApproxStride;
-display([Name,' approximate stride is: ',(num2str(Subject.MaxApproxStridecm)),' cm'])
-end
--- Matlab/GAIT GRAPHING/VelApprox.m
+++
@@ -1,9 +0,0 @@
-function[Subject] = VelApprox(Subject,T,x)
-Psum(x) = 0;
-for n = x:1:T-1
-    Psum(n+1) = abs(Subject.SpineBaseAVGcm(n+1) - Subject.SpineBaseAVGcm(n)) + Psum(n);
-end
-Subject.V = Psum(T-1)/(T-x);
-Subject.Vb = .3 * Subject.V;
-display(Subject.Vb);
-end
