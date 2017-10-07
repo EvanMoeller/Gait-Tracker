b3a4778191e435a0c519152a45b82d5eb6003b84
maxnussbaum <maxnussbaum@live.com>
2017-10-07 16:08:58 -0400

    Added method for Arm Angles
    File now has methods for Angles for Knee-Ankle and Elbow-Hand

--- PythonCode/angles.py
+++ PythonCode/angles.py
@@ -9,8 +9,12 @@ objer = Graphing("walter1.csv")
 legAngles = []
 for i in range(len(objer.xKneeLeft)):
     legAngles.append(math.atan((objer.xKneeLeft[i]-objer.xAnkleLeft[i])/(objer.yKneeLeft[i]-objer.yAnkleLeft[i])))
-print(legAngles)
 plt.plot(legAngles)
+plt.show()
+armAngles = []
+for i in range(len(objer.xElbowLeft)):
+    armAngles.append(math.atan((objer.xElbowLeft[i]-objer.xHandLeft[i])/(objer.yElbowLeft[i]-objer.yHandLeft[i])))
+plt.plot(armAngles)
 plt.show()
 """
 arctan((xKneeLeft-xAnkleLeft)/(yKneeLeft-yAnkleLeft))
